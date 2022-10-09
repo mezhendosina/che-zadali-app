a0ee387e6cfb587bfb758fc5a2e3ea75dc5caf9a
mezhendosina <mensh.zheya@gmail.com>
2022-10-09 09:17:42 +0500

    debug

--- app/src/main/java/com/mezhendosina/sgo/app/ui/journalItem/HomeworkAdapter.kt
+++ app/src/main/java/com/mezhendosina/sgo/app/ui/journalItem/HomeworkAdapter.kt
@@ -3,7 +3,7 @@ package com.mezhendosina.sgo.app.ui.journalItem
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -61,7 +61,7 @@ class HomeworkAdapter(
 
     override fun onClick(v: View) {
         val lesson = v.tag as LessonUiEntity
-        val view = v.rootView.findViewById<ConstraintLayout>(R.id.homework_item)
+        val view = v.rootView.findViewById<CardView>(R.id.homework_item)
 
         onHomeworkClickListener(lesson, view)
     }
