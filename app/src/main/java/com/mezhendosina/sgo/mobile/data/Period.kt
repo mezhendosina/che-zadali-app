fb565b5d22e6d976b91f28c7b47d4287d460bfbd
TheEntropyShard <theentropyshard@gmail.com>
2024-02-22 19:14:23 +0300

    feat: add some dataclasses for mobile api

---
+++ app/src/main/java/com/mezhendosina/sgo/mobile/data/Period.kt
@@ -0,0 +1,7 @@
+package com.mezhendosina.sgo.mobile.data
+
+data class Period(
+    val id: Int,
+    val periodName: String,
+    val periodType: String,
+)
---
+++ app/src/main/java/com/mezhendosina/sgo/mobile/data/YearTotal.kt
@@ -0,0 +1,5 @@
+package com.mezhendosina.sgo.mobile.data
+
+data class YearTotal(
+    val period: Period,
+)
